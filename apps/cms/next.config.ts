a5bc714970cc84ab92e6587049e9f049da094ba7
khoinguyen <khoi.nguyen@kyanon.digital>
2026-06-17 23:19:11 +0700

    t

--- apps/cms/next.config.ts
+++ apps/cms/next.config.ts
@@ -2,7 +2,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["cms.dounus.id.vn", "pc-3000.dounus.id.vn"],
+  allowedDevOrigins: ["cms.dounus.id.vn"],
 };
 
 export default withPayload(nextConfig);
